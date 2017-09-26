b68011c3c7f5a3590a5d2d3458dd5787ec5f17a9
irianto <email": null>
2017-09-26 19:47:10 +0700

    frontend register, login, logout

--- client/app.js
+++ client/app.js
@@ -3,7 +3,19 @@ var app = new Vue({
   data: {
     items: [],
     cart: [],
-    totalPriceCart: 0
+    totalPriceCart: 0,
+    token: '',
+    registerState: {
+      name: '',
+      email: '',
+      username: '',
+      password: ''
+    },
+    loginState: {
+      username: '',
+      password: ''
+    },
+    loginMsg: ''
   },
   methods: {
     getAllItems () {
@@ -22,6 +34,16 @@ var app = new Vue({
 
     showCart () {
       $('#cart-modal').modal()
+    },
+
+    showLogin () {
+      $('#register-modal').modal('hide')
+      $('#login-modal').modal()
+    },
+
+    showRegister () {
+      $('#login-modal').modal('hide')
+      $('#register-modal').modal()
     },
 
     addToCart (item) {
@@ -63,29 +85,102 @@ var app = new Vue({
 
     checkOut () {
       console.log('ini method checkOut');
-      this.cart.forEach(itemCart => {
-        // console.log(itemCart.name);
-        this.items.forEach(item => {
-          if (itemCart._id == item._id) {
-            item.stock--
+      if (this.token == '') {
+        $('#cart-modal').modal('hide')
+        this.showLogin()
+      }
+      else {
+        this.cart.forEach(itemCart => {
+          // console.log(itemCart.name);
+          this.items.forEach(item => {
+            if (itemCart._id == item._id) {
+              item.stock--
 
-            axios({
-              method: 'put',
-              url: `http://localhost:3000/items/${item._id}/stock`,
-              data: {
-                stock: item.stock
-              }
-            })
-            .then(response => {
-              console.log(response.data.msg);
-            })
-            .catch(err => console.log(err))
+              axios({
+                method: 'put',
+                url: `http://localhost:3000/items/${item._id}/stock`,
+                data: {
+                  stock: item.stock
+                }
+              })
+              .then(response => {
+                console.log(response.data.msg);
+              })
+              .catch(err => console.log(err))
 
-          }
+            }
+          })
         })
+        this.clearCart()
+        $('#cart-modal').modal('hide')
+      }
+    },
+
+    logout () {
+      this.cart = []
+      this.token = ''
+      localStorage.removeItem('ecommercetoken')
+    },
+
+    login () {
+      this.loginMsg = ''
+      console.log('ini method login');
+      let self = this
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/signin',
+        data: {
+          username: self.loginState.username,
+          password: self.loginState.password
+        }
       })
-      this.clearCart()
-      $('#cart-modal').modal('hide')
+      .then(response => {
+        console.log(response);
+        self.loginState = {
+          username: '',
+          password: ''
+        }
+        self.loginMsg = response.data.msg
+
+        if (response.data.msg == 'login success') {
+          localStorage.setItem('ecommercetoken', response.data.data)
+          self.token = localStorage.getItem('ecommercetoken')
+          $('#login-modal').modal('hide')
+          $('#login-form')[0].reset()
+          if (self.cart.length != 0) {
+            self.showCart()
+          }
+        }
+      })
+      .catch(err => console.log(err))
+    },
+
+    register () {
+      console.log('ini method register');
+      let self = this
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/signup',
+        data: {
+          name: self.registerState.name,
+          email: self.registerState.email,
+          username: self.registerState.username,
+          password: self.registerState.password
+        }
+      })
+      .then(response => {
+        console.log(response);
+        $('#register-modal').modal('hide')
+        $('#register-form')[0].reset()
+
+        self.registerState = {
+          name: '',
+          email: '',
+          username: '',
+          password: ''
+        }
+      })
+      .catch(err => console.log(err))
     }
   },
   created () {
