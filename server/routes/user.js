dcffe922ea402c2d37f0ab2f637acbd7dc814b6c
irianto <email": null>
2017-10-12 20:28:29 +0700

    tambah .env, matikan endpoint create user

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ const auth = require('../helpers/auth')
 
 router.get('/', auth.isLogin, auth.isAdmin, user.getAllUsers)
 router.get('/:id', auth.isLogin, auth.isOwner, user.getUsersById)
+// router.post('/', user.createUser)
 router.post('/', auth.isLogin, auth.isAdmin, user.createUser)
 router.delete('/:id', auth.isLogin, auth.isAdmin, user.deleteUser)
 router.put('/:id', auth.isLogin, auth.isOwner, user.editUser)
